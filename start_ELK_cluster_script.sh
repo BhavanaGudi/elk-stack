c951afa9c7af2bd081061e9b747af793d9c764bd
BhavanaGudi <bhav.gudi@gmail.com>
2019-08-26 22:49:44 +0530

    commenting local images

--- start_ELK_cluster_script.sh
+++ start_ELK_cluster_script.sh
@@ -1,6 +1,6 @@
 apk add git
 git clone https://github.com/BhavanaGudi/elk-stack.git
 cd /root/elk-stack/elk-docker
-docker build -t tomcat_local tomcat/. & docker build -t logstash_local logstash/. & docker build -t kibana_local kibana/. & docker build -t nginx_local nginx/.
+#docker build -t tomcat_local tomcat/. & docker build -t logstash_local logstash/. & docker build -t kibana_local kibana/. & docker build -t nginx_local nginx/.
 docker network create mynet
 docker-compose up -d
